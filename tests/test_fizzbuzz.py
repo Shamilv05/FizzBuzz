e30ade18cca8c48644d31de419efb58d3d707840
Shamil <Vagabov>
2019-05-06 21:09:54 +0300

    Test was improved

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -19,6 +19,10 @@ def test_fizzbuzz_30():
     assert(fizzbuzz(30) == "fizzbuzz")
 
 
+def test_fizzbuzz_60():
+    assert(fizzbuzz(60) == "fizzbuzz")
+
+
 def test_fizzbuzz_17():
     assert(fizzbuzz(17) == ":( 17 :(")
 
